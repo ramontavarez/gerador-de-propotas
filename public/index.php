1fb4d387319f8e494076e621078eea476c2378b7
Ramon Tavares <ramon.six@hotmail.com>
2016-07-18 00:58:10 -0300

    testando mudança  no path

--- public/index.php
+++ public/index.php
@@ -21,6 +21,11 @@
 
 require __DIR__.'/../bootstrap/autoload.php';
 
+// set the public path to this directory
+$app->bind('path.public', function() {
+    return __DIR__;
+});
+
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
